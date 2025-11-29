494e681206c2cd1104b41bbfeed42159ad1fc8e4
Juyeong "James" Noh <juyeongnoh@gmail.com>
2025-11-30 05:11:10 +0900

    chore: motion 설치

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ async function startServer() {
     await sequelize.authenticate();
     console.log("✅ 데이터베이스 연결 성공");
 
-    await sequelize.sync({ alter: true });
+    await sequelize.sync();
     console.log("✅ 데이터베이스 동기화 완료");
 
     app.listen(PORT, () => {
