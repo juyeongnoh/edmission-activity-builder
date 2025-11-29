389a012859d2195bc04a506f1f066d9e1486ea0f
Juyeong "James" Noh <juyeongnoh@gmail.com>
2025-11-29 23:01:38 +0900

    feat: Impace Score 계산 함수 구현

---
+++ frontend/src/lib/calulateImpactScore.js
@@ -0,0 +1,31 @@
+const calulateImpactScore = (tier, isLeadership, hours) => {
+  let score = 0;
+
+  switch (tier) {
+    case "School":
+      score += 1;
+      break;
+    case "Regional":
+      score += 2;
+      break;
+    case "State":
+      score += 3;
+      break;
+    case "National":
+      score += 4;
+      break;
+    case "International":
+      score += 5;
+      break;
+    default:
+      break;
+  }
+
+  if (isLeadership) score += 2;
+
+  if (hours > 10) score += 1;
+
+  return score;
+};
+
+export { calulateImpactScore };
